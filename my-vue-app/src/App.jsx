2ee28219926560b91ab69bdb52e4076e1f970a0a
Daniel Valderrama <danivva95@gmail.com>
2022-12-15 18:52:29 +0100

    updated useState

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,11 +1,17 @@
-// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import './App.css'
-import Navbar from './assets/Header/header'
+import {Navbar} from './assets/Header/Navbar'
+import { useState } from 'react'
+function Likes() {
+  const [likes, setLikes] = useState(0);
+  return <button onClick={() => setLikes(likes + 1)}>{likes} likes </button>;
+}
 const menu = ["All products", "Boku No Hero Academia", "Kingdom Hearts", "About us", "Deals", "My Orders"]
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  
 
   return (
     <div className="App">
@@ -13,6 +19,7 @@ function App() {
       <Navbar menuItems={menu}/>
       {/* <Home/>
       <Footer/> */}
+      <Likes/>
     </div>
   )
 }
--- my-vue-app/src/assets/Header/Navbar.jsx
+++ my-vue-app/src/assets/Header/Navbar.jsx
@@ -1,4 +1,4 @@
-export default function Navbar ({menuItems}) {
+export function Navbar ({menuItems}) {
     return (
         <nav>
       <div className="container navbar">
