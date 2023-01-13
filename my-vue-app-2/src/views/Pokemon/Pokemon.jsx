d6d7345e2b5edac30829af575f4632e0a1198514
Daniel Valderrama Arrabal <danivva95@gmail.com>
2023-01-13 13:07:54 +0100

    added pokeapi

--- my-vue-app-2/src/views/Pokemon/Pokemon.jsx
+++ my-vue-app-2/src/views/Pokemon/Pokemon.jsx
@@ -1,5 +1,56 @@
-export default function Pokemon () {
-    return (
-        <h2 className="text-white">Pokemon Category</h2>
-    )
+import { useEffect, useState } from "react";
+
+export default function Pokemon() {
+  const [newPokemon, setNewPokemon] = useState("");
+  const [pokemons, setPokemon] = useState([]);
+
+  useEffect(
+    function () {
+      async function fetchPokemon() {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemons}`
+        );
+        const details = await response.json();
+        setPokemon(details);
+      }
+      fetchPokemon();
+    },
+    [pokemons]
+  );
+
+  function addNewPokemon(event) {
+    setNewPokemon(event.target.value);
+    //console.log(event.target.value);
+  }
+
+  function addPokemon(event) {
+    event.preventDefault();
+
+    if (newPokemon === "") {
+      return;
+    }
+    setPokemon(newPokemon);
+    setNewPokemon("");
+  }
+
+  return (
+    <div className="App">
+      <h1>PokeApi</h1>
+      <form onSubmit={addPokemon}>
+        <input value={newPokemon} onChange={addNewPokemon}></input>
+        <button>Search Pokemon</button>
+      </form>
+
+      {!pokemons ? (
+        <h3>No Pokemon</h3>
+      ) : (
+        <ul>
+          <li>
+            {pokemons.name}
+            <img src={pokemons.sprites.front_default} alt="" />
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 }
