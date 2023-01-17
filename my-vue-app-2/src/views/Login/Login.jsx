88ea58bd92fa869e9cd409a99782ecdbd8d98c95
Daniel Valderrama <danivva95@gmail.com>
2023-01-17 18:11:47 +0100

    completed login

--- my-vue-app-2/src/views/Login/Login.jsx
+++ my-vue-app-2/src/views/Login/Login.jsx
@@ -1,19 +1,20 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState} from "react";
+import {useNavigate} from "react-router-dom"
+const initialUserState = {
+  email: "",
+  password: "",
+  name: "",
+  surname: "",
+}
 
 export default function Login() {
   const [credentials, setCredentials] = useState({
     email: "",
     password: "",
   });
+ const navigate = useNavigate()
 
-  const [newUser, setNewUser] = useState({
-    email:"",
-    password:"",
-    name: "",
-    surname:""
-  })
-  console.log(credentials);
+  const [newUser, setNewUser] = useState(initialUserState);
 
   function handleCredentials(event) {
     const newCredentials = {
@@ -34,94 +35,101 @@ export default function Login() {
   }
 
   async function signIn(e) {
-    
+    e.preventDefault();
 
-    e.preventDefault()
+    fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUser),
+    }).then((response) => {
+      console.log(response.status);
+      if (response.status == 401) {
+        throw "No autorizado";
+      } else if (response.status === 200) {
+        alert(`User ${newUser.name} signed-in successfully`);
+        setNewUser(initialUserState)
+      } else if (response.status === 409) {
+        alert(`Usuario ya registrado`);
+      }
+    });
+  };
 
+  async function logIn(e) {
+    e.preventDefault();
 
-    fetch("http://localhost:8000/login", {
-        method: "POST",
-        headers: {"Content-Type":"application/json"},
-        body: JSON.stringify(newUser)
-
-    }).then((response)=> {
-        console.log(response.status);
-        if(response.status == 401) {
-            throw "No autorizado"
-        } else if (response.status === 200) {
-          alert (`User ${newUser.name} signed-in successfully`)
-        } else if (response.status === 200) {
-          alert (`Body error`)
-        }
-
-    })};
-  
-
-  function handleSubmit(event, credentials) {
-    event.preventDefault();
-    if (
-      credentials.email === "pepe@mail.com" &&
-      credentials.password === "hola"
-    ) {
-      alert("Logged in");
-    } else {
-      alert("Wrong credentials");
-    }
-
-    console.log(credentials);
+    fetch("http://localhost:3000/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    }).then((response) => {
+      console.log(response.status);
+      if (response.status == 401) {
+        throw "No autorizado";
+      } else if (response.status === 200) {
+        navigate("/")
+      } else {
+        alert("Invalid user or password, try again")
+      }
+    });
   }
+
+  
 
   return (
     <>
-    <div>
-      <h2 className="text-white">Esto es el Login</h2>
-      <form onSubmit={(event) => handleSubmit(event, credentials)}>
-        <input
-          type="email"
-          name="email"
-          value={credentials.email}
-          onChange={handleCredentials}
-        />
-        <input
-          type="password"
-          name="password"
-          value={credentials.password}
-          onChange={handleCredentials}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      <div>
+        <h2 className="text-white">Esto es el Login</h2>
+        <form onSubmit={logIn}>
+          <input
+            type="email"
+            name="email"
+            value={credentials.email}
+            onChange={handleCredentials}
+          />
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleCredentials}
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
 
-<div>
-<h2 className="text-white">Esto es el SignIn</h2>
-<form onSubmit={(event) => signIn(event, newUser)}>
-  <input
-    type=""
-    name="name"
-    value={newUser.surname}
-    onChange={handleInput}
-  />
-  <input
-    type="email"
-    name="email"
-    value={newUser.name}
-    onChange={handleInput}
-  />
-  <input
-    type="email"
-    name="email"
-    value={newUser.email}
-    onChange={handleInput}
-  />
-  <input
-    type="password"
-    name="password"
-    value={newUser.password}
-    onChange={handleInput}
-  />
-  <button type="submit">Login</button>
-</form>
-</div>
-</>
+      <div>
+        <h2 className="text-white">Esto es el SignIn</h2>
+        <form onSubmit={signIn}>
+          <input
+            type="text"
+            name="name"
+            placeholder="nombre"
+            value={newUser.name}
+            onChange={handleInput}
+          />
+          <input
+            type="text"
+            name="surname"
+            placeholder="apellidos"
+            value={newUser.surname}
+            onChange={handleInput}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={newUser.email}
+            onChange={handleInput}
+          />
+          <input
+            type="password"
+            placeholder="pass"
+            name="password"
+            value={newUser.password}
+            onChange={handleInput}
+          />
+          <button type="submit">Sign-In</button>
+        </form>
+      </div>
+    </>
   );
 }
